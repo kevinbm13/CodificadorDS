ca4594324a8832e9ff738cd9a77358031d9759ae
Daniel <Daniel@G4-2082LA>
2016-09-29 22:08:05 -0600

    Algoritmos Daniel

---
+++ Proyecto01/Proyecto01/Transposicion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto01
+{
+    class Transposicion : Algoritmo
+    {
+        public override string codificar(string abecedario, string tiraEntrada, string tipoAlgoritmo)
+        {
+            String tiraSalida = "";
+            for (int x = tiraEntrada.Length - 1; x >= 0; x--)
+            {
+                tiraSalida += tiraEntrada[x];
+            }
+            
+            Console.WriteLine(tiraSalida);
+            return tiraSalida;
+        }
+
+        public override string decodificar(string abecedario, string tiraEntrada, string tipoAlgoritmo)
+        {
+            String tiraSalida = "";
+            for (int x = tiraEntrada.Length - 1; x >= 0; x--)
+            {
+                tiraSalida += tiraEntrada[x];
+
+            }
+
+            Console.WriteLine(tiraSalida);
+            return tiraSalida;
+            
+        }
+    }
+}
---
+++ Proyecto01/Proyecto01/TransposicionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto01
+{
+    class TransposicionFactory : AlgoritmoFactory
+    {
+        public Algoritmo crearAlgoritmo()
+        {
+            Transposicion transposicion = new Transposicion();
+
+            return transposicion;
+        }
+    }
+}
