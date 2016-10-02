963b612fe4df2cfaabaf4db62baca2caf4e173eb
Kevin <kevinbm13@gmail.com>
2016-10-02 15:16:50 -0600

    Kevin

--- Proyecto01/Proyecto01/Program.cs
+++ Proyecto01/Proyecto01/Program.cs
@@ -11,7 +11,8 @@ namespace Proyecto01
     {
         static void Main(string[] args)
         {
-
+            //Application.EnableVisualStyles();
+            //Application.Run(new Proyecto());
             ControladorConsola c = new ControladorConsola();
             c.ejecutar();
         }
